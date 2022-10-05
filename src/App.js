5e659f3ea8b4f4e0ef4255cf1e98f4f4d19e47ac
KietHo1 <kiethostartup1999@gmail.com>
2022-10-05 22:15:52 +0700

    Edit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <body>
-          <h1>Sign here:</h1>
-          <canvas width="600" height="300"></canvas>
-          <div class="button-container">
-            <button>Clear</button>
-          </div>
-          <script src="hello.js"></script>
-        </body>
       </header>
     </div>
   );
