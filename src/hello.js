d7a5fadfc3744dd842c956bf88169b31a5ff1cd4
KietHo1 <kiethostartup1999@gmail.com>
2022-10-05 15:22:04 +0700

    Edit

---
+++ src/hello.js
@@ -0,0 +1,61 @@
+const canvas = document.querySelector("canvas");
+const form = document.querySelector(".signature-website-form");
+const clearButton = document.querySelector(".clear-button");
+const ctx = canvas.getContext("2d");
+let writingMode = false;
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const imageURL = canvas.toDataURL();
+    const image = document.createElement("img");
+    image.src = imageURL;
+    image.height = canvas.height;
+    image.width = canvas.width;
+    image.style.display = "block";
+    form.appendChild(image);
+    clearPad();
+})
+
+const clearPad = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+clearButton.addEventListener("click", (event) =>{
+    event.preventDefault();
+    clearPad();
+})
+
+const getTargetPosition = (event) =>{
+    const positionX = event.clientX - event.target.getBoundingClientRect().x;
+    const positionY = event.clientY - event.target.getBoundingClientRect().y;
+
+    return [positionX, positionY];
+}
+
+const handlePointerMove = (event) => {
+    if(!writingMode) return;
+
+    const [positionX, positionY] = getTargetPosition(event);
+    ctx.lineTo(positionX, positionY);
+    ctx.stroke();
+}
+
+const handlePointerUp = (event) => {
+    writingMode = false;
+}
+
+const handlePointerDown = (event) => {
+    writingMode = true;
+    ctx.beginPath();
+
+    const [positionX, positionY] = getTargetPosition(event);
+    ctx.moveTo(positionX, positionY);
+}
+
+ctx.lineWidth = 3;
+ctx.lineJoin = ctx.lineCap = "round";
+
+canvas.addEventListener("pointerdown", handlePointerDown, {passive: true});
+canvas.addEventListener("pointerup", handlePointerUp, {passive: true}); 
+canvas.addEventListener("pointermove", handlePointerMove, {passive: true});
